a33081d4fc6dc653fe024fb8166325e80f0e22d5
Alexander Escalona Fernández <alexescalonafernandez@gmail.com>
2026-07-13 18:17:05 +0200

    Fix queue consumer local startup telemetry

--- src/SecureAppServiceApiLite.QueueConsumer/Program.cs
+++ src/SecureAppServiceApiLite.QueueConsumer/Program.cs
@@ -1,5 +1,6 @@
 using Azure.Monitor.OpenTelemetry.Exporter;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Azure.Functions.Worker.OpenTelemetry;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OpenTelemetry;
@@ -11,9 +12,19 @@ builder.ConfigureFunctionsWebApplication();
 
 builder.Services.AddSingleton<QueueMessageProcessor>();
 
-builder.Services
+var openTelemetryBuilder = builder.Services
     .AddOpenTelemetry()
-    .UseFunctionsWorkerDefaults()
-    .UseAzureMonitorExporter();
+    .UseFunctionsWorkerDefaults();
+
+var applicationInsightsConnectionString =
+    builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"];
+
+if (!string.IsNullOrWhiteSpace(applicationInsightsConnectionString))
+{
+    openTelemetryBuilder.UseAzureMonitorExporter(options =>
+    {
+        options.ConnectionString = applicationInsightsConnectionString;
+    });
+}
 
 builder.Build().Run();
