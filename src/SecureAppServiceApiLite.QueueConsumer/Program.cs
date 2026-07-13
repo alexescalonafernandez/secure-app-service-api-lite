3c3f87d039fb31f7f7b103f40430224dfe40b361
Alexander Escalona Fernández <alexescalonafernandez@gmail.com>
2026-07-13 18:26:50 +0200

    Conditionally enable Azure Monitor exporter and use `dotnet run` task (#27)
    * Fix queue consumer local startup telemetry
    
    * refactor: remove unused namespaces

--- src/SecureAppServiceApiLite.QueueConsumer/Program.cs
+++ src/SecureAppServiceApiLite.QueueConsumer/Program.cs
@@ -1,8 +1,8 @@
 using Azure.Monitor.OpenTelemetry.Exporter;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Azure.Functions.Worker.OpenTelemetry;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OpenTelemetry;
 using SecureAppServiceApiLite.QueueConsumer.Processing;
 
 var builder = FunctionsApplication.CreateBuilder(args);
@@ -11,9 +11,19 @@ builder.ConfigureFunctionsWebApplication();
 
 builder.Services.AddSingleton<QueueMessageProcessor>();
 
-builder.Services
+var openTelemetryBuilder = builder.Services
     .AddOpenTelemetry()
-    .UseFunctionsWorkerDefaults()
-    .UseAzureMonitorExporter();
+    .UseFunctionsWorkerDefaults();
+
+var applicationInsightsConnectionString =
+    builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"];
+
+if (!string.IsNullOrWhiteSpace(applicationInsightsConnectionString))
+{
+    openTelemetryBuilder.UseAzureMonitorExporter(options =>
+    {
+        options.ConnectionString = applicationInsightsConnectionString;
+    });
+}
 
 builder.Build().Run();
